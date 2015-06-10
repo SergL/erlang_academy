278d748336e1a08f11f3dbcfeb84d8db966a0ad1
SergL <serg_l@ukr.net>
2015-06-10 10:05:34 +0300

    Update p11.erl

--- lesson_01/p11.erl
+++ lesson_01/p11.erl
@@ -2,22 +2,17 @@
 -export([encode_modified/1]).
 
 encode_modified(List) ->
-    encode_modified(p05:reverse(List), 1, [], []).
+    p05:reverse(encode_modified(List, 1, [], [])).
 
 encode_modified([], _N, [], Acc) ->
     Acc;
-
 encode_modified([Head|Tail], N, [], _Acc) ->
     encode_modified(Tail, N, [Head], []);
-
 encode_modified([Head|Tail], N, [Head]=[Tmp], Acc) ->
     encode_modified(Tail, N + 1, [Head], Acc);
-
 encode_modified([Head|Tail], N=1, [Tmp], Acc) ->
     encode_modified(Tail, 1, [Head], [Tmp|Acc]);
-
 encode_modified([Head|Tail], N, [Tmp], Acc) ->
     encode_modified(Tail, 1, [Head], [{N, Tmp}|Acc]);
-
 encode_modified([], N=1, [Tmp], Acc) ->
     encode_modified([], N, [], [Tmp|Acc]).
